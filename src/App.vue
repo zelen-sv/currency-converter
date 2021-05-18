acefe51853e49b2a5fa0ecf3bc5b7c6e19d1a85d
zelen-sv <st.zelenko@gmail.com>
2021-05-18 20:02:36 +0300

    final

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,151 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="currency-converter" :class="{'currency-converter--loading': loading}">
+    <div class="currency-converter__form">
+      <div class="currency-converter__field">
+        <input v-model.number="fromValue"
+               @input="updateToValue"
+               type="number"
+               class="currency-converter__input">
+
+        <currency-picker v-model="currencyFrom"
+                         :currencies="currencies"
+                         @change="updateFromValue" />
+      </div>
+
+      <div class="currency-converter__field">
+        <input v-model.number="toValue"
+               @input="updateFromValue"
+               type="number"
+               class="currency-converter__input">
+
+        <currency-picker v-model="currencyTo"
+                         :currencies="currencies"
+                         @change="updateToValue" />
+      </div>
+
+      <button class="currency-converter__sync" @click="sync">
+        Sync
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import axios from 'axios';
+  import CurrencyPicker from '@/components/CurrencyPicker.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  const CURRENCIES_LIST = ['BTC','USD','EUR'],
+        API_TOKEN = '34ec37bc31cfc816df251fb1e2a6b9f97506e53a1614fe455673e96b15941b3c',
+        API_URL = 'https://min-api.cryptocompare.com/data/pricemulti',
+        SYNC_TIME = 1000 * 60;
+
+  export default {
+    components: {
+      CurrencyPicker
+    },
+    data () {
+      return {
+        currencies: CURRENCIES_LIST,
+        currencyFrom: CURRENCIES_LIST[0],
+        currencyTo: CURRENCIES_LIST[0],
+        fromValue: 1,
+        toValue: 1,
+        currenciesApiData: null,
+        loading: false,
+        syncer: null
+      }
+    },
+    created () {
+      this.fetchCurrenciesData()
+      this.syncer = setInterval(() => this.sync(), SYNC_TIME);
+    },
+    beforeDestroy() {
+        clearInterval(this.syncer);
+    },
+    methods: {
+      async fetchCurrenciesData () {
+        const url = `${API_URL}?api_key=${API_TOKEN}&fsyms=${CURRENCIES_LIST}&tsyms=${CURRENCIES_LIST}`;
+        this.loading = true;
+        let { data } = await axios.get(url);
+        this.currenciesApiData = data
+        this.loading = false;
+      },
+      updateToValue () {
+        this.toValue = this.fromValue * this.currenciesApiData[this.currencyFrom][this.currencyTo]
+      },
+      updateFromValue () {
+        this.fromValue = this.toValue * this.currenciesApiData[this.currencyTo][this.currencyFrom]
+      },
+      async sync () {
+        await this.fetchCurrenciesData();
+        this.updateToValue();
+      }
+    }
+  };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0;
+    background-color: $dark;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: $white;
+  }
+
+  .currency-converter {
+    background-color: $grey;
+    border-radius: 4px;
+    padding: 40px;
+  }
+
+  .currency-converter--loading {
+    opacity: 0.5;
+    cursor: progress;
+    pointer-events: none;
+  }
+
+  .currency-converter__field {
+    margin-bottom: 30px;
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .currency-converter__input {
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 20px;
+    color: white;
+    background-color: $dark;
+    border-radius: 4px;
+    border: none;
+    padding: 10px 20px;
+    box-sizing: border-box;
+  }
+
+  .currency-converter__sync {
+    width: 100%;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 20px;
+    padding: 10px 20px;
+    box-sizing: border-box;
+    background-color: $orange;
+    color: $white;
+    border-radius: 4px;
+    border: none;
+    cursor: pointer;
+    text-transform: uppercase;
+
+    position: relative;
+    bottom: 0;
+
+    &:hover {
+      bottom: 3px;
+      transition: bottom 0.3s ease;
+    }
+  }
 </style>
---
+++ src/components/CurrencyPicker.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="currency-picker">
+    <div v-for="item in currencies"
+         :key="`currency-${item}`"
+         class="currency-picker__currency"
+         :class="{'currency-picker__currency--active': item == modelValue }"
+         @click="updateCurrency(item)">
+      {{ item }}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    model: {
+      prop: 'modelValue',
+      event: 'change'
+    },
+    props: {
+      currencies: {
+        type: Array,
+        required: true
+      },
+      modelValue: {
+        type: String,
+        required: true
+      }
+    },
+    methods: {
+      updateCurrency (item) {
+        this.$emit('change', item)
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .currency-picker {
+    display: flex;
+    margin-left: 30px;
+  }
+
+  .currency-picker__currency {
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 20px;
+    padding: 10px 20px;
+    background-color: $dark;
+    cursor: pointer;
+
+    &:first-child {
+      border-top-left-radius: 4px;
+      border-bottom-left-radius: 4px;
+    }
+
+    &:last-child {
+      border-top-right-radius: 4px;
+      border-bottom-right-radius: 4px;
+    }
+  }
+
+  .currency-picker__currency--active {
+    border: 1px solid $orange;
+    color: $orange;
+  }
+</style>
